459d32a15822c5440e03d5863b94e46eeeff0eb4
Hooveer <mofmasterchauncey@gmail.com>
2024-04-23 19:14:45 +0800

    Temporary Intent on MainActivity

--- app/src/main/java/com/example/gigver/MainActivity.java
+++ app/src/main/java/com/example/gigver/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.gigver;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.List;
 
@@ -80,6 +83,17 @@ public class MainActivity extends AppCompatActivity {
                     public void OnFailure(String errorMessage) {
                         System.out.println(errorMessage);
                     }
+        });
+
+        //Added a temporary Intent object so that it can run the 2nd page (Profile Page)
+        //Subject to change
+        Button submitButton =(Button)findViewById(R.id.submitButton);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),ProfilePage.class);
+                startActivity(intent);
+            }
         });
     }
 }
