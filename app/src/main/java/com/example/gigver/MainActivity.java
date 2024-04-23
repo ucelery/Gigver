d9c6f992c64984a7f3cdaf2ae07978495274fc33
Aton Alwin A. David <atondavid.ad@gmail.com>
2024-04-23 20:18:57 +0800

    Remove ServerManager Use Cases

--- app/src/main/java/com/example/gigver/MainActivity.java
+++ app/src/main/java/com/example/gigver/MainActivity.java
@@ -21,69 +21,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        ServerManager server = new ServerManager("https://gigver-server.onrender.com");
-
-        // Gets Users
-        server.GetUsers(new IServerEvent<List<User>>() {
-            @Override
-            public void OnComplete(List<User> result) {
-                for (User user : result) {
-                    System.out.println("Hello User, " + user.GetName());
-                }
-            }
-
-            @Override
-            public void OnFailure(String errorMessage) {
-                System.out.println(errorMessage);
-            }
-        });
-
-        // Gets Posts
-        server.GetPosts(new IServerEvent<List<Post>>() {
-            @Override
-            public void OnComplete(List<Post> result) {
-                for (Post post : result) {
-                    System.out.println("Theres a post about: " + post.GetSubject());
-                }
-            }
-
-            @Override
-            public void OnFailure(String errorMessage) {
-                System.out.println(errorMessage);
-            }
-        });
-
-        // Add User
-        User newUser = new User("Jhob", "jabalos@mymail.mapua.edu.ph", "Some Random Password", "123 Ordinary St. Madeup City", "09394567777", "02 87000");
-
-        server.AddUser(newUser,
-                new IServerEvent<User>() {
-            @Override
-            public void OnComplete(User result) {
-                System.out.println("Newly Added User: " + result.GetID());
-            }
-
-            @Override
-            public void OnFailure(String errorMessage) {
-                System.out.println(errorMessage);
-            }
-        });
-
-        // Add Post
-        Post newPost = new Post("662604696f7ea20e4cd6b23e", "Delivery Nearby", "Some stuff needed to be delivered by foot", "Monetary");
-
-        server.AddPost(newPost,
-                new IServerEvent<Post>() {
-                    @Override
-                    public void OnComplete(Post result) {
-                        System.out.println("Newly Added Post: " + result.GetID());
-                    }
-
-                    @Override
-                    public void OnFailure(String errorMessage) {
-                        System.out.println(errorMessage);
-                    }
-        });
         //Added a temporary Intent object so that it can run the 2nd page (Profile Page)
         //Subject to change
         Button submitButton =(Button)findViewById(R.id.submitButton);
