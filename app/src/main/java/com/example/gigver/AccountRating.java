a9f1ab00d2940538bf48c4e2373f7356dde2a087
Hooveer <mofmasterchauncey@gmail.com>
2024-04-23 20:57:55 +0800

    Added Account Rating Page

--- app/src/main/java/com/example/gigver/AccountRating.java
+++ app/src/main/java/com/example/gigver/AccountRating.java
@@ -2,14 +2,13 @@ package com.example.gigver;
 
 import android.os.Bundle;
 
-
 import androidx.appcompat.app.AppCompatActivity;
 
-public class ProfilePage extends AppCompatActivity {
+public class AccountRating extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile_page);
+        setContentView(R.layout.activity_account_rating);
     }
 }
