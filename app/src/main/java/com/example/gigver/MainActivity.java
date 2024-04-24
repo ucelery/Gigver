1467fe26c760d4456c1dbf13009fd013be49b23c
Hooveer <mofmasterchauncey@gmail.com>
2024-04-24 18:12:41 +0800

    Implemented a Listener in the Home page for email and password input

--- app/src/main/java/com/example/gigver/MainActivity.java
+++ app/src/main/java/com/example/gigver/MainActivity.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.List;
@@ -26,6 +29,9 @@ public class MainActivity extends AppCompatActivity {
         //Subject to change
         Button submitButton =(Button)findViewById(R.id.submitButton);
         TextView create = (TextView) findViewById(R.id.createAccount);
+        TextView enterEmail = (TextView) findViewById(R.id.editEmail);
+        TextView passwordTextView = findViewById(R.id.passwordTextView);
+        EditText passwordEditText = findViewById(R.id.passwordEditText); //This is set to invisible, this is where the user types his/her password
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -38,6 +44,44 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(),CreateAccount.class);
                 startActivity(intent);
+            }
+        });
+        //Implements a listener that if the email TextView is clicked, automatically deletes the word Email
+        enterEmail.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus && enterEmail.getText().toString().equals("Email")) {
+                    enterEmail.setText("");
+                }
+            }
+        });
+        //Implements a listener that if the password TextView is clicked, automatically deletes the word password
+        passwordTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Hide the TextView
+                passwordTextView.setVisibility(View.INVISIBLE);
+                // Show the EditText
+                passwordEditText.setVisibility(View.VISIBLE);
+                // Request focus on the EditText
+                passwordEditText.requestFocus();
+            }
+        });
+        passwordEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+                // If the EditText is empty, switch back to TextView
+                if (s.length() == 0) {
+                    passwordEditText.setVisibility(View.INVISIBLE);
+                    passwordTextView.setVisibility(View.VISIBLE);
+                    passwordTextView.setText("Password");
+                }
             }
         });
     }
