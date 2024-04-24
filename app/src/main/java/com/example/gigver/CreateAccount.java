f2e8d6499add7ce0bbd8839559290058841ecc48
Hooveer <mofmasterchauncey@gmail.com>
2024-04-24 13:49:48 +0800

    Added Create Account Page

---
+++ app/src/main/java/com/example/gigver/CreateAccount.java
@@ -0,0 +1,13 @@
+package com.example.gigver;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+public class CreateAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
--- app/src/main/java/com/example/gigver/MainActivity.java
+++ app/src/main/java/com/example/gigver/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -24,10 +25,18 @@ public class MainActivity extends AppCompatActivity {
         //Added a temporary Intent object so that it can run the 2nd page (Profile Page)
         //Subject to change
         Button submitButton =(Button)findViewById(R.id.submitButton);
+        TextView create = (TextView) findViewById(R.id.createAccount);
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(),ProfilePage.class);
+                startActivity(intent);
+            }
+        });
+        create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),CreateAccount.class);
                 startActivity(intent);
             }
         });
