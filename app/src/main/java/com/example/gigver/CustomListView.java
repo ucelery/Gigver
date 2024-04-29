8218d1a171f41cd8cc679ffc1ebef2320c6d7455
Hooveer <mofmasterchauncey@gmail.com>
2024-04-29 08:00:48 +0800

    Added Listview on Home Feed

---
+++ app/src/main/java/com/example/gigver/CustomListView.java
@@ -0,0 +1,14 @@
+package com.example.gigver;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CustomListView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_list_view);
+    }
+}
